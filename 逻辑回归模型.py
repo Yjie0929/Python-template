ea0e2b33c5e7a34467cc3ba125208e1c7ae694cc
LyaMa <83082953+Yjie0929@users.noreply.github.com>
2023-03-14 22:25:01 +0800

    Update 逻辑回归模型.py
    数据来自sk-learn提供的乳腺癌数据

--- 逻辑回归模型.py
+++ 逻辑回归模型.py
@@ -13,22 +13,22 @@ X_test = X[400:]  # take the last 169 samples as the features of the test set
 y_test = y[400:]  # take the last 169 samples as the labels of the test set
 
 # Create and train a logistic regression model
-model = LogisticRegression(max_iter=500,
-                           solver="newton-cg")  # create a logistic regression object, you can specify parameters such as regularization term, learning rate etc., default using L2 regularization and liblinear solver.
-model.fit(X_train,
-          y_train)  # use the features and labels of the training set to fit the model, i.e. solve for model parameters.
+model = LogisticRegression(max_iter=500, solver="newton-cg")  
+# create a logistic regression object, you can specify parameters such as regularization term, learning rate etc., default using L2 regularization and liblinear solver.
+model.fit(X_train, y_train)  
+# use the features and labels of the training set to fit the model, i.e. solve for model parameters.
 
 # Predict on test set and calculate accuracy score
 y_pred = model.predict(X_test)  # use model to predict on test set features, get prediction results.
-acc = accuracy_score(y_test,
-                     y_pred)  # use accuracy_score function to calculate accuracy score between prediction results and true labels, i.e. proportion of correct predictions.
+acc = accuracy_score(y_test, y_pred)  
+# use accuracy_score function to calculate accuracy score between prediction results and true labels, i.e. proportion of correct predictions.
 print("Accuracy score:", acc)  # print accuracy score.
 
 # Plot comparison between prediction results and true labels
-plt.scatter(range(len(y_test)), y_test,
-            label="True labels")  # use scatter function to plot scatter plot, x-axis is sample index number, y-axis is true label (0 or 1), and set legend.
-plt.scatter(range(len(y_pred)), y_pred,
-            label="Prediction results")  # same as above, y-axis is prediction result (0 or 1).
+plt.scatter(range(len(y_test)), y_test, label="True labels")  
+# use scatter function to plot scatter plot, x-axis is sample index number, y-axis is true label (0 or 1), and set legend.
+plt.scatter(range(len(y_pred)), y_pred, label="Prediction results")  
+# same as above, y-axis is prediction result (0 or 1).
 plt.ylim(0, 2) # set the display range of Y axis to 0-10
 plt.legend()  # show legend.
 plt.show()  # show image.
