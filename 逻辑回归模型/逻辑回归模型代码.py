0cc314025a08d60c308c26b247822153e2c8528a
LyaMa <83082953+Yjie0929@users.noreply.github.com>
2023-03-14 23:55:36 +0800

    Update 逻辑回归模型代码.py

--- 逻辑回归模型/逻辑回归模型代码.py
+++ 逻辑回归模型/逻辑回归模型代码.py
@@ -1,34 +1,51 @@
-# import numpy as np  # for scientific computing
-# import pandas as pd  # for data processing and analysis
-import matplotlib.pyplot as plt  # for data visualization
-from sklearn.datasets import load_breast_cancer  # breast cancer dataset
-from sklearn.linear_model import LogisticRegression  # logistic regression model
-from sklearn.metrics import accuracy_score  # accuracy evaluation
+# Import necessary libraries
+import numpy as np # Mathematical operations library
+import pandas as pd # Data processing library
+import matplotlib.pyplot as plt # Data visualization library
+from sklearn.datasets import load_breast_cancer # Breast cancer dataset
+from sklearn.linear_model import LogisticRegression # Logistic regression class
+from sklearn.metrics import accuracy_score, confusion_matrix # Model evaluation metrics
+from sklearn.preprocessing import StandardScaler # Data standardization class
 
-# Load the dataset and split it into training set and test set
-X, y = load_breast_cancer(return_X_y=True)  # X is the feature matrix, y is the label vector
-X_train = X[:400]  # take the first 400 samples as the features of the training set
-y_train = y[:400]  # take the first 400 samples as the labels of the training set
-X_test = X[400:]  # take the last 169 samples as the features of the test set
-y_test = y[400:]  # take the last 169 samples as the labels of the test set
+# Load the dataset and check basic information
+data = load_breast_cancer() # Return a dictionary object containing data and meta-information
+X = data['data'] # Feature matrix X (569*30)
+y = data['target'] # Target variable y (569*1), 0 means malignant, 1 means benign
+feature_names = data['feature_names'] # Feature names (30)
+target_names = data['target_names'] # Target names (2)
+print('Data shape:', X.shape)
+print('Feature names:', feature_names)
+print('Target names:', target_names)
 
-# Create and train a logistic regression model
-model = LogisticRegression(max_iter=500, solver="newton-cg")  
-# create a logistic regression object, you can specify parameters such as regularization term, learning rate etc., default using L2 regularization and liblinear solver.
-model.fit(X_train, y_train)  
-# use the features and labels of the training set to fit the model, i.e. solve for model parameters.
+# Visualize part of the dataset (only select the first two features for plotting)
+plt.scatter(X[y==0, 0], X[y==0, 1], c='b', label=target_names[0])
+plt.scatter(X[y==1, 0], X[y==1, 1], c='r', label=target_names[1])
+plt.xlabel(feature_names[0])
+plt.ylabel(feature_names[1])
+plt.legend()
+plt.show()
 
-# Predict on test set and calculate accuracy score
-y_pred = model.predict(X_test)  # use model to predict on test set features, get prediction results.
-acc = accuracy_score(y_test, y_pred)  
-# use accuracy_score function to calculate accuracy score between prediction results and true labels, i.e. proportion of correct predictions.
-print("Accuracy score:", acc)  # print accuracy score.
+# Split the dataset into training set and test set (ratio of 8:2)
+np.random.seed(0) # Set random seed for reproducibility of results
+indices = np.random.permutation(len(X)) # Randomly shuffle index values
+train_size = int(len(X) * 0.8) # Training set size (80%)
+X_train = X[indices[:train_size]] # Training set feature matrix (455*30)
+y_train = y[indices[:train_size]] # Training set target variable (455*1)
+X_test = X[indices[train_size:]] # Test set feature matrix (114*30)
+y_test = y[indices[train_size:]] # Test set target variable (114*1)
 
-# Plot comparison between prediction results and true labels
-plt.scatter(range(len(y_test)), y_test, label="True labels")  
-# use scatter function to plot scatter plot, x-axis is sample index number, y-axis is true label (0 or 1), and set legend.
-plt.scatter(range(len(y_pred)), y_pred, label="Prediction results")  
-# same as above, y-axis is prediction result (0 or 1).
-plt.ylim(0, 2) # set the display range of Y axis to 0-10
-plt.legend()  # show legend.
-plt.show()  # show image.
+# Standardize the data to make each feature have zero mean and unit variance
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Use the LogisticRegression class in sklearn.linear_model to fit and predict the model, and increase the number of iterations max_iter to 1000, choose solver as 'newton-cg'
+lr = LogisticRegression(max_iter=1000, solver='newton-cg')
+lr.fit(X_train_scaled, y_train)
+y_pred = lr.predict(X_test_scaled)
+
+# Calculate the accuracy and confusion matrix of the model and print the results
+acc = accuracy_score(y_test, y_pred)
+cm = confusion_matrix(y_test, y_pred)
+print('Accuracy:', acc)
+print('Confusion matrix:\n', cm)
